8b8995b14601334aa74726f13f538390ad436fea
pablodiazcg <pablodiazcg7@gmail.com>
2026-09-07 18:47:58 -0500

    Update 9.7 app.py

--- app.py
+++ app.py
@@ -147,11 +147,13 @@ TOUR_MEMBERSHIP = {
     "Rodolfo Cazaubon":       "Y",   # PGA Tour Americas
     "Jose Cristobal Islas":   "Y",   # PGA Tour Americas
     "José Cristóbal Islas":    "Y",
+    "José Cristóbal Islas":    "Y",
     "Luis Carrera":           "Y",   # PGA Tour Americas
     "Sebastian Vazquez":      "Y",   # PGA Tour Americas
     "Julio Arronte":          "Y",   # PGA Tour Americas
     "Marcelo Garza":          "Y",   # PGA Tour Americas
     "Jose Cristobal Islas":   "Y",   # PGA Tour Americas
+    "José Cristóbal Islas":    "Y",
     "José Cristóbal Islas":    "Y",
 }
 
@@ -460,6 +462,7 @@ PLAYER_IDS = {
     "Abraham Ancer":            "45526",
     "Marcelo Garza":            "65558",
     "Jose Cristobal Islas":     "66282",
+    "José Cristóbal Islas":     "66282",
     "José Cristóbal Islas":     "66282",
     "José de Jesús Rodríguez":  "Y",
 }
